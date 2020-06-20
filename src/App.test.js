0845c7fec87c2c874e96fd88ccddc1493ba353e3
Victor Poeisz <victorpoeisz@google.com>
2020-06-19 20:30:25 -0700

    add jest to project

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,7 @@
 /*
  * The MIT License (MIT)
  *
- * Copyright (c) 2019 Looker Data Sciences, Inc.
+ * Copyright (c) 2020 Looker Data Sciences, Inc.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -22,40 +22,10 @@
  * THE SOFTWARE.
  */
 
-module.exports = api => {
-  var isTest = api.cache(() => process.env.NODE_ENV === "test")
 
-  return {
-    ignore: ['node_modules'],
-    env: {
-      build: {
-        ignore: [
-          '**/*.d.ts',
-          '**/*.test.js',
-          '**/*.test.jsx',
-          '**/*.test.ts',
-          '**/*.test.tsx',
-          '__snapshots__',
-          '__tests__',
-        ],
-      },
-    },
-    presets: [
-      ['@babel/preset-env',
-        {targets: {
-            browsers: 'Last 2 Chrome versions, Firefox ESR',
-            node: '8.9'}}],
-      ['@babel/preset-react', {development: process.env.BABEL_ENV !== 'build'}],
-      '@babel/preset-typescript'
-    ],
-    plugins: [
-      '@babel/plugin-proposal-class-properties',
-      '@babel/plugin-proposal-object-rest-spread',
-      '@babel/plugin-transform-runtime',
-      '@babel/plugin-proposal-optional-chaining',
-      '@babel/plugin-proposal-nullish-coalescing-operator',
-      'babel-plugin-styled-components',
-      'react-hot-loader/babel'
-    ],
-  }
-}
+ // Just putting something here to stub out the jest pipeline
+describe("Jest bootstrap", () => {
+    test("runs a test that passes", () => {
+        expect(true).toBeTruthy()
+    })
+})
